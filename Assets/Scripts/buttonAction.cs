010542f2068a7365f0b6ddb0c0a89b137b72905e
black-tiger340602 <niklaussergei23@gmail.com>
2023-09-01 05:10:27 -0700

    first

---
+++ Assets/Scripts/buttonAction.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class buttonAction : MonoBehaviour
+{
+    public GameObject menu = null;
+    public GameObject panel = null;
+    public TMP_InputField planetid = null;
+    public GameObject[] planets;
+    
+    public void onClickMenu(){
+        panel.SetActive(true);
+        
+    }
+
+    public void onClickOkButton(){
+        panel.SetActive(false);
+    }
+
+    public void onClickAddButton(){
+        //Debug.Log(planetid.text);
+        try{
+            int id = Convert.ToInt32(planetid.text);
+            if(id > 10 || id < 0){
+                return;
+            }
+            id--;
+            planets[id].SetActive(true);
+        }catch(Exception e){
+            Debug.Log(e.ToString());
+        }
+        
+    }
+}
